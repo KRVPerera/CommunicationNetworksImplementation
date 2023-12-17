281152e1285e941a2521be454212c91123d73c86
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2023-12-17 04:16:25 +0200

    starter

--- Exercise2/start_services.sh
+++ Exercise2/start_services.sh
@@ -7,11 +7,7 @@ sudo service grafana-server start
 cd ~/projects/cn1-impl-opt-coap
 
 # Start the CoAP server
-cd server
-source cn1_server/bin/activate
-python3 server.py
+(cd server && source cn1_server/bin/activate && python3 server.py &)
 
 
-cd ../client
-source cn1_client/bin/activate
-python3 client.py
+(cd client && source cn1_client/bin/activate && python3 client.py)

be2d0fc9504e79b7ff65c45a41e929426241fe7c
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2023-12-17 04:03:16 +0200

    start servers
    Signed-off-by: KRVPerera <2173530+KRVPerera@users.noreply.github.com>

---
+++ Exercise2/start_services.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+sudo systemctl start influxdb
+sudo service grafana-server start
+
+cd ~/projecs/cn1-impl-opt-coap
+
+# Start the CoAP server
+cd server
+source cn1_server/bin/activate
+python3 server.py
+
+
+cd ../client
+source cn1_client/bin/activate
+python3 client.py
